b316c8daf67a19a3e75014536f4927a910d7561a
Poty <potyfoliva@hotmail.com>
2020-12-10 19:23:22 -0300

    projeto inicial

---
+++ app-web-paroquia/src/main/java/br/edu/ifsp/arq/projeto/appwebparoquia/AppWebParoquiaApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ifsp.arq.projeto.appwebparoquia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppWebParoquiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppWebParoquiaApplication.class, args);
+	}
+
+}
---
+++ app-web-paroquia/src/test/java/br/edu/ifsp/arq/projeto/appwebparoquia/AppWebParoquiaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ifsp.arq.projeto.appwebparoquia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppWebParoquiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
